d4ecea70a78289f938852092f62d5822d96d9667
Shariful Islam <nomad-mac@Mds-Mac-mini.local>
2020-11-03 20:28:06 +0600

    Add discovery client

--- ribbon-rest/src/main/java/com/nomad/riborekaclient/controller/HomeController.java
+++ ribbon-rest/src/main/java/com/nomad/riborekaclient/controller/HomeController.java
@@ -3,9 +3,13 @@ package com.nomad.riborekaclient.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class HomeController {
@@ -13,7 +17,17 @@ public class HomeController {
     public static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
     private RestTemplate restApp;
+
+    @GetMapping("/service-info")
+    public int serviceInfo(){
+        logger.info("ribbon-rest: service-info is called...");
+        List<ServiceInstance> instanceInfos = this.discoveryClient.getInstances("rest-app");
+        return instanceInfos.size();
+    }
 
     @GetMapping("/get-info")
     public String getInfo(){
